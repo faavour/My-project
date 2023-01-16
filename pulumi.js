ef6c7adb150139bbcc4abe9f80f9ac40fdb7be07
faavour <favourjohn862@gmail.com>
2023-01-16 17:00:08 +0100

    provisioned a digital ocean cluster

--- pulumi.js
+++ pulumi.js
@@ -1,2 +1,12 @@
 const pulumi = require("@pulumi/pulumi");
 const digitalocean = require("@pulumi/digitalocean");
+
+// This Create a new DigitalOcean Kubernetes cluster
+const cluster = new digitalocean.KubernetesCluster("my-cluster", {
+    region: "nyc1",
+    version: "1.19",
+    nodePool: {
+        name: "default",
+        size: "s-1vcpu-2gb",
+        nodeCount: 2,
+    },
