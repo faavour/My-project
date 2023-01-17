37326ac3a89efb656843b520323fd3aca45d5e2e
faavour <favourjohn862@gmail.com>
2023-01-17 07:26:25 +0100

    added deployment

--- index.ts
+++ index.ts
@@ -14,3 +14,31 @@ const cluster = new digitalocean.KubernetesCluster("do-cluster", {
 });
 
 export const kubeconfig = cluster.kubeConfigs[0].rawConfig;
+
+const provider = new kubernetes.Provider("do-k8s", { kubeconfig })
+
+const appLabels = { "app": "app-nginx" };
+const app = new kubernetes.apps.v1.Deployment("do-app-dep", {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 5,
+        template: {
+            metadata: { labels: appLabels },
+            spec: {
+                containers: [{
+                    name: "nginx",
+                    image: "nginx",
+                }],
+            },
+        },
+    },
+}, { provider });
+const appService = new kubernetes.core.v1.Service("do-app-svc", {
+    spec: {
+        type: "LoadBalancer",
+        selector: app.spec.template.metadata.labels,
+        ports: [{ port: 80 }],
+    },
+}, { provider });
+
+export const ingressIp = appService.status.loadBalancer.ingress[0].ip;
